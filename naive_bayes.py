eddd741094b7b54b9ceccc760fab468ba11e28fd
jordi.vilaseca.corderroure <jordi.vilaseca.corderroure@est.fib.upc.edu>
2015-12-12 12:13:59 +0100

    Added KFold cross-validation to naive bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -4,35 +4,32 @@ import pandas as pd
 import math
 import random
 from sklearn.naive_bayes import BernoulliNB
+from sklearn import cross_validation
 
 PIZZA_FIELD = '@@requester_received_pizza@@'
 
 df = pd.read_csv('output.csv', delim_whitespace=True)
 
-# Divide data frame to features and targets
-features = df.ix[:,1:len(df.columns)].values
-targets = df.ix[:,-1].values
+# Divide data frame to feature and target
+feature = df.ix[:,0:len(df.columns)-1].values
+target = df.ix[:,-1].values
 
 # Total number of entries
-N = len(targets)
+N = len(target)
 
-# Divide data to learning and testing set, where the learning has 2/3 of the data.
-# learning/testing contain the indices of elements that are part of the learning/testing set.
-learning = random.sample(range(N),math.floor(2*N/3))
-testing = list(set(range(N))-set(learning))
-
-nlearning = len(learning)
-ntesting = len(testing)
-
-# Train Naive Bayes with the training set
 clf = BernoulliNB()
-clf.fit(features[learning], targets[learning])
 
-# Predict the testing set data
-prediction = clf.predict(features[testing])
+#Simple K-Fold cross validation. 10 folds.
+cv = cross_validation.KFold(N, n_folds=10)
+results = []
+for traincv, testcv in cv:
+    model = clf.fit(feature[traincv], target[traincv])
+    prediction = model.predict(feature[testcv])
 
-# Count the number of errors.
-diff = targets[testing] != prediction
-nerrors = sum(diff)
-percError = nerrors/ntesting
-print('Percentage of error:', round(percError*100,2))
+    # Count the number of errors.
+    diff = target[testcv] != prediction
+    nerrors = sum(diff)
+    percError = nerrors/len(prediction)
+    results.append(percError)
+
+print ("Results:", str(np.array(results).mean()))

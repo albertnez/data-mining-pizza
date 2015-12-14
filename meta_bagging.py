166b9ec7090fb515a8ce389c3732de6490679223
jordi.vilaseca.corderroure <jordi.vilaseca.corderroure@est.fib.upc.edu>
2015-12-14 12:34:52 +0100

    meta_bagging: fixed decision stump, added number of estimators

--- meta_bagging.py
+++ meta_bagging.py
@@ -5,8 +5,8 @@ from sklearn.tree import DecisionTreeClassifier
 
 # Decission tree
 print('Decision tree:')
-classifier_report(BaggingClassifier(DecisionTreeClassifier()))
+classifier_report(BaggingClassifier(base_estimator=DecisionTreeClassifier()))
 
 # Decission stumps
 print('Decision stump:')
-classifier_report(BaggingClassifier(DecisionTreeClassifier(max_depth=2)))
+classifier_report(BaggingClassifier(base_estimator=DecisionTreeClassifier(max_depth=1),n_estimators=100))

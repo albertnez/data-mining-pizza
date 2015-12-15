8ebf43affd4abd7f51ee7ea856dda0f17fa66a2e
Miquel Llobet <mllobet.cm@gmail.com>
2015-12-15 02:06:09 +0100

    Added svm code

--- svm.py
+++ svm.py
@@ -2,4 +2,22 @@
 from classifier import classifier_report
 from sklearn.svm import SVC
 
-classifier_report(SVC())
+# Polykernel
+print('Testing Polykernel')
+for d in range(1, 4):
+    for c in range(-2, 3):
+        cost = 10**c
+        print('starting: degree = {0}, cost = {1}'.format(d, cost))
+        classifier_report(SVC(kernel='poly', degree=d, C=cost))
+        print('finished: degree = {0}, cost = {1}'.format(d, cost))
+
+
+# Gaussian RBF Kernel
+print('Testing RBF')
+for g in range(-2, 3):
+    for c in range(-2, 3):
+        cost = 10**c
+        gamma = 10**g
+        print('starting: gamma = {0}, cost = {1}'.format(gamma, cost))
+        classifier_report(SVC(kernel='rbf', gamma=gamma, C=cost))
+        print('finished: gamma = {0}, cost = {1}'.format(gamma, cost))
